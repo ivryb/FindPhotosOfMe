a03832380e15ee67782a842844c5cda63488617b
Ivan Rybnikov <ivrybn@gmail.com>
2025-10-03 12:20:43 +0300

    shadcn-vue installed

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -1,19 +1,35 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
-	compatibilityDate: "latest",
-	devtools: { enabled: true },
-	modules: ["@nuxt/ui", "convex-nuxt"],
-	css: ["~/assets/css/main.css"],
-	devServer: {
-		port: 3001,
-	},
-	ssr: true,
-	convex: {
-		url: process.env.NUXT_PUBLIC_CONVEX_URL,
-	},
-	runtimeConfig: {
-		public: {
-			serverURL: process.env.NUXT_PUBLIC_SERVER_URL,
-		},
-	},
+  compatibilityDate: "latest",
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss", "convex-nuxt", "shadcn-nuxt"],
+  css: ["~/assets/css/tailwind.css"],
+  devServer: {
+    port: 3001,
+  },
+  ssr: true,
+  convex: {
+    url: process.env.NUXT_PUBLIC_CONVEX_URL,
+  },
+  runtimeConfig: {
+    public: {
+      serverURL: process.env.NUXT_PUBLIC_SERVER_URL,
+    },
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "./components/ui",
+  },
 });
