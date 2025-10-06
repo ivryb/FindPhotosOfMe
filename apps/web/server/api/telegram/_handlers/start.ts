20a8a5a9a4c3e8067422fbc1ea071a8a8e980d8c
Ivan Rybnikov <ivrybn@gmail.com>
2025-10-06 23:31:37 +0300

    markdown?

--- apps/web/server/api/telegram/_handlers/start.ts
+++ apps/web/server/api/telegram/_handlers/start.ts
@@ -3,15 +3,20 @@ import type { Doc } from "@FindPhotosOfMe/backend/convex/_generated/dataModel";
 
 const getText = (collection: Doc<"collections">) => {
   return `
-Hi! This bot will help you find photos of you from the IT Arena!\n\n
-There are ${collection.imagesCount} photos in the collection.\n\n
-Just send here a photo of yourself, and we will use machine learning magic to filter out photos where you are present.\n\n
-P.S. Your reference photo will not be stored or shared with anyone. Pinky promise!
+Hi! This bot will help you *find photos of yourself from the IT Arena!* 🙌🏻
+
+There are currently *${collection.imagesCount} photos* in the collection.  
+
+Just *send a photo of yourself here* 📸 and we will use some machine learning magic 💫 to filter out the photos where you are present.  
+
+P.S. Your reference photo will not be stored or shared with anyone. _Pinky promise!_  
 `.trim();
 };
 
 export const handleStart = (collection: Doc<"collections">) => {
   return async (ctx: Context) => {
-    await ctx.reply(getText(collection));
+    await ctx.reply(getText(collection), {
+      parse_mode: "MarkdownV2",
+    });
   };
 };
